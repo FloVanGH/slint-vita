5ae39d66ed7abe9455f941d95ee9881a40160568
Florian Blasius <florian.blasius@slint-ui.com>
2023-03-20 12:51:44 +0100

    cleanup

--- node/bubble.controller.js
+++ node/bubble.controller.js
@@ -3,38 +3,7 @@ let slint = require("slint-ui");
 class BubbleController {
     constructor(service, window) {
         this.service = service;
-
-        var bubbles = service.bubbles;
-
-        // let pages = {
-        //     rowCount() { return bubbles.length; },
-        //     rowData(row) { return bubbles[row]; },
-        //     setRowData(row, data) { bubbles[row] = data; this.notify.rowDataChanged(row); },
-        // };
-
-        console.log(bubbles.length);
-
-        let model = new slint.ArrayModel([]);
-        bubbles.forEach(page => {
-            let pageModel = [];
-
-            console.log(page.length);
-
-            page.forEach(bubble => {
-                pageModel.push({
-                    title: bubble.title,
-                    row: bubble.row,
-                    column: bubble.column,
-                    filler: bubble.filler,
-                    icon: bubble.icon
-                });
-            });
-
-            model.push(pageModel);
-        });
-        console.log(model);
-
-        window.bubbles = model;
+        window.bubbles = new slint.ArrayModel(service.bubbles);
     }
 }
 
