7e9293aa15efdaa07d10f2e1ff73c2c603b531e1
Florian Blasius <florian.blasius@slint-ui.com>
2023-03-27 11:50:15 +0200

    display page from js

--- node/controllers/page_controller.js
+++ node/controllers/page_controller.js
@@ -1,6 +1,30 @@
+let slint = require("slint-ui");
+
 class PageController {
     constructor(service, window) {
         this.service = service;
+        this.window = window;
+
+        window.open_page.setHandler(this.openPage);
+        window.close_page.setHandler(this.closePage);
+
+        this.displayPages();
+    }
+
+    displayPages() {
+        this.window.pages = new slint.ArrayModel(this.service.pages);
+    }
+
+    openPage = (id) => {
+        this.service.openPage(id);
+        this.displayPages();
+
+        return true;
+    }
+
+    closePage = (id) => {
+        this.service.closePage(id);
+        this.displayPages();
     }
 }
 
--- node/services/page_service.js
+++ node/services/page_service.js
@@ -1,10 +1,22 @@
+class Page {
+    constructor(title, color) {
+        this.id = title;
+        this.title = title;
+        this.color = color;
+    }
+}
+
 class PageService {
     constructor() {
-
+        this.pages = [];
     }
 
-    get pages() {
-        return [];
+    openPage(id) {
+        this.pages.push(new Page(id, "#000000"));
+    }
+
+    closePage(id) {
+        this.pages = this.pages.filter(p => p.id == id);
     }
 }
 
