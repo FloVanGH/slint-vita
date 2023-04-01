62de4b23653035ecc05697803a60f81168f7f930
Florian Blasius <florian.blasius@slint-ui.com>
2023-04-01 11:41:37 +0200

    work on list selection move

--- src/controllers/list_controller.ts
+++ src/controllers/list_controller.ts
@@ -2,11 +2,13 @@
 // SPDX-License-Identifier: MIT
 
 import { type MainWindow, slint } from "../../ui-import";
+import { type ListItem } from "../data/list";
 import { type ListService } from "../services/interfaces/list_service";
 
 export class ListController {
     private readonly _service: ListService;
     private readonly _mainWindow: MainWindow;
+    private _selectedItem?: ListItem;
 
     constructor(service: ListService, mainWindow: MainWindow) {
         this._service = service;
@@ -20,4 +22,26 @@ export class ListController {
         }
         this._mainWindow.list_items = listItemsModel;
     }
+
+    private clearSelection(): void {
+        if (this._selectedItem === undefined) {
+            return;
+        }
+
+        this._selectedItem.selected = false;
+        this._selectedItem = undefined;
+    }
+
+    private select(listItem?: ListItem): boolean {
+        if (listItem === undefined || this._selectedItem === listItem) {
+            return false;
+        }
+
+        this.clearSelection();
+        return true;
+    }
+
+    moveUp = (): void => {
+        console.log("blub");
+    };
 }
